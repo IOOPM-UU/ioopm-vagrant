8d2332fc73e23685dc71206cbaff7836785db1d2
Stephan Brandauer <stephan.brandauer@it.uu.se>
2016-08-29 14:22:18 +0200

    use official IOOPM emacs config

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -6,10 +6,14 @@ sudo echo "127.0.0.1 ubuntu-xenial" >> /etc/hosts
 sudo apt-get update
 
 ### emacs
+sudo apt-get install -y unzip
 sudo apt-get install -y emacs24-nox
 
-# start and quit emacs to install spacemacs
-# emacs --insecure -f kill-emacs
+wget http://wrigstad.com/ioopm/emacs-setup-linux.zip
+mkdir .emacs.d
+unzip emacs-setup-linux.zip -d .emacs.d
+sudo chown -R vagrant .emacs.d
+rm emacs-setup-linux.zip
 
 #### programming tools
 sudo apt-get install -y git build-essential gdb gdb clang lldb valgrind libcunit1 libcunit1-dev libcunit1-doc
